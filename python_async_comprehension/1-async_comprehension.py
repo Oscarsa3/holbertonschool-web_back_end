fcf160a9cc0dcc8fff5db33cf72ce6d754b12b55
Oscarsa3 <oscarsadev@gmail.com>
2023-09-01 17:04:21 +0000

    fix: annotated type

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,11 +2,11 @@
 """will collect 10 random numbers using an
 async comprehensing over async_generator"""
 import asyncio
-from typing import List, Any
+from typing import List
 async_ = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[Any]:
+async def async_comprehension() -> List[float]:
     """return the 10 random numbers."""
     aleatorio = [i async for i in async_()]
     return aleatorio
