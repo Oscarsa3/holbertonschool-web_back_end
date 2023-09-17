5bca681df801995d8a17c6aad776a5c889a1fd4f
Oscarsa3 <oscarsadev@gmail.com>
2023-09-17 22:26:16 +0000

    feat: add getter and setter

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -12,6 +12,16 @@ export default class Currency {
 
   // set method code
   set code(value) {
+    this._code = value;
+  }
+
+  // get method to name
+  get name() {
+    return this._name;
+  }
+
+  // set method name
+  set name(value) {
     this._name = value;
   }
 
