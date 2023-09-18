f94d85c494dc713d1d86e6d174b9b075fcb3b4a1
Oscarsa3 <oscarsadev@gmail.com>
2023-09-18 01:19:13 +0000

    fix: export

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-class HolbertonClass {
+export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -18,7 +18,7 @@ class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-class StudentHolberton {
+export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
