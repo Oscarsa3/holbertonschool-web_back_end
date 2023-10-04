1d6642b6db376995ae218f9d9c4a9ac0997f6c9a
Oscarsa3 <oscarsadev@gmail.com>
2023-10-04 17:42:46 +0000

    fix: update

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,14 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photo = uploadPhoto();
-  const user = createUser();
-
-  Promise.all([photo, user])
+  Promise.all([uploadPhoto(), createUser()])
     .then((resolve) => {
       console.log(`${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    }, () => { console.log('Signup system offline') });
 }
