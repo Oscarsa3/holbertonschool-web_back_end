2d211791f7b1f5f1dea291a3283983c6962d45ab
Oscarsa3 <oscarsadev@gmail.com>
2023-10-04 16:55:20 +0000

    feat: add a method that print a promise

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  Promise.all([photo, user])
+    .then((resolve) => {
+      console.log(resolve[0].body, resolve[1].firstName, resolve[1].lastName);
+    }, () => {
+      console.log('Signup system offline');
+    });
+}
