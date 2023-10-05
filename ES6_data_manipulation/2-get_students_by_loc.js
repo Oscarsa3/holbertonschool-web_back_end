0c3136748f27cec83103eb7f523cd069998f7972
Oscarsa3 <oscarsadev@gmail.com>
2023-10-05 20:38:16 +0000

    feat: add Filter

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,8 @@
+export default function getStudentsByLocation(students, city) {
+  const select = students.filter((std) => {
+    if (std.location === city) {
+      return std;
+    }
+  });
+  return select;
+}
