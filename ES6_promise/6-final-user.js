3ffdf4ce64c068047995cdf2175a66b7f491c150
Oscarsa3 <oscarsadev@gmail.com>
2023-10-05 17:04:25 +0000

    fix: update

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((resolves) => {
       resolves.forEach((resolve) => {
-        arr.push({ status: resolve.status, value: resolve.status === 'fulfilled' ? resolve.value : resolve.reason });
+        arr.push({ status: resolve.status, value: resolve.status === 'fulfilled' ? resolve.value : `${resolve.reason.name}: ${resolve.reason.message}` });
       });
       return arr;
     }).catch((err) => err);
