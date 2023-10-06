d35791084c7d47edfcc2dc2290b0a21ce85007bf
Oscarsa3 <oscarsadev@gmail.com>
2023-10-06 06:19:00 +0000

    fix: add conditional about some error

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -2,5 +2,8 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
   view.setInt8(position, value);
+  if (position < 0 || position >= length) {
+   throw new Error('Position outside range');
+  }
   return view;
 }
